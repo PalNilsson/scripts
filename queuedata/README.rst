176bdd383075c8ab5fda66182590de8086754875
Paul Nilsson <paul.nilsson@cern.ch>
2023-03-10 12:32:34 +0100

    First commit

---
+++ queuedata/README.rst
@@ -0,0 +1,3 @@
+This script will download the queuedata for a given PanDA queue from the PanDA server.
+
+Usage: ./queuedata.sh [-h] [-q queue name]
---
+++ queuedata/queuedata.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+WORKDIR=$PWD
+
+usage="$(basename "$0") [-h] [-q queue name] -- download PanDA queuedata for the given queue
+
+where:
+    -h  show this help text
+    -q  PanDA queue name"
+
+if [[ $1 == "" ]]; then
+  echo $usage
+  exit 1
+fi
+
+while getopts :hq: flag
+do
+    case "${flag}" in
+        h) echo $usage
+          exit 0
+          ;;
+        q) queue=${OPTARG};;
+        :) printf "missing argument for -%s\n" "$OPTARG" >&2
+          echo "$usage" >&2
+          exit 1
+          ;;
+        \?) printf "illegal option: -%s\n" "$OPTARG" >&2
+          echo "$usage" >&2
+          exit 1
+          ;;
+    esac
+done
+
+if [[ $queue != "" ]]; then
+  curl -sS "http://pandaserver.cern.ch:25085/cache/schedconfig/$queue.all.json" >$queue.json
+fi
